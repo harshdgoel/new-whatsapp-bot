9c631b79e527ecf27c946127585325ee6f7f4fee
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-11 16:01:53 +0530

    Update botController.js

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -1,13 +1,19 @@
-// controller/botController.js
-
+// controllers/botController.js
 const { sendResponseToWhatsApp } = require("../services/apiHandler");
-const stateMachine = require("../states/stateMachine");
+const stateMachine = require("../states/stateMachine");  // This should import the instance, not the class
 
 const handleIncomingMessage = async (phoneNumberId, from, message) => {
     const { body, intent } = message;
+    console.log("Received message:", message);
+
+    // Ensure handleMessage exists
+    if (typeof stateMachine.handleMessage !== 'function') {
+        throw new Error("handleMessage is not a function on stateMachine");
+    }
+
+    // Call handleMessage
     const responseMessage = await stateMachine.handleMessage(from, body, intent);
     await sendResponseToWhatsApp(phoneNumberId, from, responseMessage);
 };
 
 module.exports = { handleIncomingMessage };
-     
