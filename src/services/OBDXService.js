0daf3f7e0545488ae017e3342392c528a729d80b
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-12 13:53:06 +0530

    Update OBDXService.js

--- src/services/OBDXService.js
+++ src/services/OBDXService.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const axios = require('axios');
-const baseURL = "https://rnqgj-148-87-23-5.a.free.pinggy.link"; // Ensure this is your correct base URL
+const baseURL = "https://rnagq-148-87-23-5.a.free.pinggy.link"; // Ensure this is your correct base URL
 
 class OBDXService {
     // Accept LoginService as a parameter
