78a93d0f86dd881e347113f8bf39cde87be45c40
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-21 15:58:03 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,6 @@ module.exports = {
     whatsappToken: process.env.MYTOKEN,
     phoneNumberId: process.env.PHONE_NUMBER_ID,
     baseURL: process.env.BASE_URL,
-    defaultHomeEntity: process.env.DEFAULT_HOME_ENTITY
+    defaultHomeEntity: process.env.DEFAULT_HOME_ENTITY,
+    channel: process.env.CHANNEL
 };
