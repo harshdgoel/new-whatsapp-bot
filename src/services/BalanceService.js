564bab733ad84732f6c2d90707d91109502162ba
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-21 16:00:42 +0530

    Update BalanceService.js

--- src/services/BalanceService.js
+++ src/services/BalanceService.js
@@ -2,8 +2,8 @@ const TemplateLayer = require("./TemplateLayer");
 const OBDXService = require("./OBDXService");
 const LoginService = require("./loginService");
 const endpoints = require("../config/endpoints");
-
-
+const config = require("../config/config"); // Import config.js
+const channel = config.channel;
 const states = {
     OTP_VERIFICATION: "OTP_VERIFICATION",
     LOGGED_IN: "LOGGED_IN",
@@ -66,7 +66,7 @@ const templateData = {
     sections:sections,
     bodyText: "Please select an account to view details.",
     buttonText: "View Accounts",
-    channel: "whatsapp",
+    channel: channel,
     to: "916378582419", // Replace with actual recipient number
 };
 
