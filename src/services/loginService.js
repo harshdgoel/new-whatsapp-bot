05f14653d7b292b4052995a2c05877ac03525487
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-26 10:45:49 +0530

    Update loginService.js

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -9,7 +9,7 @@ const defaultHomeEntity = config.defaultHomeEntity;
 class LoginService {
     constructor() {
         this.authCache = { token: null, cookie: null, anonymousToken: null };
-        this.mobileNumber = "916378582419";
+        this.mobileNumber = "911212121212";
     }
 
     setAuthDetails(token, cookie) {
