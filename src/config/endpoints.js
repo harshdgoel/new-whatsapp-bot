387f07142ee32a1dd30f1b1d3ba7482d5fcb2e84
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-28 15:02:34 +0530

    Update endpoints.js

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -2,7 +2,7 @@ module.exports = {
     accounts: "/digx-common/dda/v1/demandDeposit",
     recentTransactions: "/digx-common/transactions/v1/recentTransactions",
     anonymousToken: "/digx-infra/login/v1/anonymousToken",
-    login: "/digx-infra/login/v1/login"
+    login: "/digx-infra/login/v1/login",
     me: "/digx-common/user/v1/me"
     // Add more endpoints as needed
 };
