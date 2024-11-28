21a2a71d5fafa89492cf59ac1f595142dc7677bb
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-28 15:02:24 +0530

    Update endpoints.js

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -3,5 +3,6 @@ module.exports = {
     recentTransactions: "/digx-common/transactions/v1/recentTransactions",
     anonymousToken: "/digx-infra/login/v1/anonymousToken",
     login: "/digx-infra/login/v1/login"
+    me: "/digx-common/user/v1/me"
     // Add more endpoints as needed
 };
