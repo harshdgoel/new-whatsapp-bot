2255a9a625a1a08e1ca12632f2d66418ef1d730a
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-12-02 11:40:56 +0530

    Update botController.js

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -7,8 +7,12 @@ const handleIncomingMessage = async (phoneNumberId, from, message) => {
 
     const responseMessage = await stateMachine.handleMessage(from, body, intent);
 
+    console.log("response message in botController is: ", responseMessage);
+
     // Determine channel from environment and send message accordingly
     const channel = process.env.CHANNEL;
+    console.log("channel in botController is: ", channel);
+
     await sendResponseToChannel(channel, phoneNumberId, from, responseMessage);
 };
 
