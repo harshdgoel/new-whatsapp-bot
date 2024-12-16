ccf419c52eba962734cc3c49f46a4e136ececae0
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-12-16 14:56:41 +0530

    Update botController.js

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -5,15 +5,23 @@ const handleIncomingMessage = async (phoneNumberId, from, message) => {
     const { body, intent } = message;
     console.log("Received message:", message);
 
-    const responseMessage = await stateMachine.handleMessage(from, body, intent);
+    // Get the main response
+    const mainResponse = await stateMachine.handleMessage(from, body, intent);
 
-    console.log("response message in botController is: ", responseMessage);
+    console.log("response message in botController is: ", mainResponse);
 
-    // Determine channel from environment and send message accordingly
     const channel = process.env.CHANNEL;
     console.log("channel in botController is: ", channel);
 
-    await sendResponseToChannel(channel, phoneNumberId, from, responseMessage);
+    // Send the main response
+    await sendResponseToChannel(channel, phoneNumberId, from, mainResponse);
+
+    // Check if the state is HELP (indicating that the flow is done) and send help menu
+    const userSession = stateMachine.getSession(from); // Get the user's session state
+    if (userSession.state === "HELP") { // Only send the help menu if the flow is complete
+        const helpMenu = await stateMachine.getHelpMenu();
+        await sendResponseToChannel(channel, phoneNumberId, from, helpMenu);
+    }
 };
 
 module.exports = { handleIncomingMessage };
