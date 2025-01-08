0d03e7e93e64006014209d94a5f3e9976b54674f
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2025-01-08 14:58:20 +0530

    Update OBDXService.js

--- src/services/OBDXService.js
+++ src/services/OBDXService.js
@@ -5,26 +5,34 @@ const defaultHomeEntity = process.env.DEFAULT_HOME_ENTITY;
 
 class OBDXService {
     // Constructs and returns headers for API calls
-    populateHeaders(loginService) {
+    populateHeaders(loginService,userSession) {
         const token = loginService.getToken();
         const cookie = loginService.getCookie();
 
         if (!token || !cookie) {
             throw new Error("Missing token or cookie for API call.");
+        }        
+        const headers = {
+            "Authorization": `Bearer ${token}`,
+            "Cookie": cookie,
+            "Content-Type": "application/json",
+            "X-Token-Type": "JWT",
+            "X-Target-Unit": defaultHomeEntity,
+        };
+    
+        // Add X-CHALLENGE_RESPONSE header if OTP is present
+        if (userSession?.authOTP) {
+            headers["X-CHALLENGE_RESPONSE"] = JSON.stringify({
+                otp: userSession.authOTP,
+                referenceNo: userSession.XTOKENREFNO,
+                authType: userSession.AUTH_TYPE,
+            });
         }
-
-        return new Map([
-            ["Authorization", `Bearer ${token}`],
-            ["Cookie", cookie],
-            ["Content-Type", "application/json"],
-            ["X-Token-Type", "JWT"],
-            ["X-Target-Unit", defaultHomeEntity]
-        ]);
     }
 
     // Main method to invoke services
-    async invokeService(ctxPath, method, queryParam, body, loginService) {
-        const headers = this.populateHeaders(loginService);
+    async invokeService(ctxPath, method, queryParam, body, loginService,userSession) {
+        const headers = this.populateHeaders(loginService,userSession);
         const responseData = await this.serviceMeth(ctxPath, method, headers, queryParam, body);
         return responseData;
     }
